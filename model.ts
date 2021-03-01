331523cd0ba0226079b98f3c7e50461b27d1f422
Melinda Dothan <melinda.dothan@gmail.com>
2021-03-01 13:51:04 +0100

    Restricting possible values with enums and literal types
    Leave a review

--- model.ts
+++ model.ts
@@ -2,15 +2,16 @@ let displayName: string = "Jess's standing desk";
 let inventoryType: string = "furniture";
 let trackingNumber: string = "FD123455";
 let createDate: Date = new Date();
-let originalCost: any = 425;
-originalCost = "A LOT of money!";
+let originalCost = 425;
 
 interface InventoryItem {
   displayName: string;
   inventoryType: string;
-  trackingNumber: string;
+  readonly trackingNumber: string;
   createDate: Date;
-  originalCost: number;
+  originalCost?: number;
+
+  addNote?: (note: string) => string;
 }
 
 function getInventoryItem(trackingNumber: string): InventoryItem {
@@ -21,6 +22,13 @@ function saveInventoryItem(item: InventoryItem) {}
 
 let inventoryItem = getInventoryItem(trackingNumber);
 
+let updatedInventoryItem = inventoryItem;
+
 inventoryItem.createDate = new Date();
 
-saveInventoryItem(inventoryItem);
+saveInventoryItem({
+  displayName: "MacBook Pro 15 Retina",
+  inventoryType: "computer",
+  trackingNumber: "MBP123456",
+  createDate: new Date(),
+});
